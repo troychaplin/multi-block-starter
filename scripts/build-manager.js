634b529ea4c708b2066605d9b4a35baeba160a54
troychaplin <troy.chaplin@gmail.com>
2025-02-28 14:20:09 -0500

    change: size checks

--- scripts/build-manager.js
+++
@@ -1,7 +0,0 @@
-const { execSync } = require('child_process');
-
-// Build regular blocks
-execSync('npm run build', { stdio: 'inherit' });
-
-// Build interactive blocks
-execSync('npm run build:interactive', { stdio: 'inherit' }); 
